9ec4352b0c96bc90be62442ce98769e40cfa15c4
Link-deep <link.deep@yandex.ru>
2024-08-06 23:44:28 +0500

    add new v.1.0

---
+++ main.py
@@ -0,0 +1,21 @@
+# python "download/down.py"
+# Скачиваем видео с ЮТУБ
+# следующие доработки, передавать видео куда-либо на сервер.!
+# в приложение на телевизор?
+
+
+import yt_dlp as youtube_dl
+
+    # URL видео, которое вы хотите скачать
+url = 'https://www.youtube.com/watch?v=oz-O74SmTSQ'
+
+    # Параметры для youtube_dl
+ytdl_opts = {
+        'format': 'bestvideo[height<=720]+bestaudio/best[height<=720]',  # Формат видео (720p)
+        'outtmpl': 'download/%(title)s.%(ext)s',  # Имя файла, в который будет сохранено видео
+        'noplaylist': True,  # Не загружать плейлисты, только одно видео
+    }
+
+    # Создаем объект youtube_dl
+with youtube_dl.YoutubeDL(ytdl_opts) as ydl:
+        ydl.download([url])
