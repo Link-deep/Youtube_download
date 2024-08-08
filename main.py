3df3ed088468d5d44236872974ba1eb8c2eb82b4
Link-deep <link.deep@yandex.ru>
2024-08-08 10:35:17 +0500

    add new v.1.1

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 # python "download/down.py"
+# требуется библиотеки:
+# - pip install yt-dlp
+# - pip install ffmpeg
 # Скачиваем видео с ЮТУБ
 # следующие доработки, передавать видео куда-либо на сервер.!
 # в приложение на телевизор?
@@ -7,13 +10,14 @@
 import yt_dlp as youtube_dl
 
     # URL видео, которое вы хотите скачать
-url = 'https://www.youtube.com/watch?v=oz-O74SmTSQ'
+url = 'https://www.youtube.com/watch?v=0tZ0SAcZtco'
 
     # Параметры для youtube_dl
 ytdl_opts = {
         'format': 'bestvideo[height<=720]+bestaudio/best[height<=720]',  # Формат видео (720p)
-        'outtmpl': 'download/%(title)s.%(ext)s',  # Имя файла, в который будет сохранено видео
+        'outtmpl': '%(title)s.%(ext)s',  # Имя файла, в который будет сохранено видео
         'noplaylist': True,  # Не загружать плейлисты, только одно видео
+        'socket_timeout': 30,
     }
 
     # Создаем объект youtube_dl
