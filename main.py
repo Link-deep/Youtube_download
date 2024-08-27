625f6db2c38e2db6b0f5c79e1cc8b6aa219a8d3e
Link-deep <link.deep@yandex.ru>
2024-08-28 01:07:12 +0500

    add commit for PC use

--- main.py
+++ main.py
@@ -1,25 +1,54 @@
-# python "download/down.py"
 # требуется библиотеки:
 # - pip install yt-dlp
 # - pip install ffmpeg
+# при работе в PyCharm и запуске на ПК треубется установить ffmpeg и настроить пусть к папке.
 # Скачиваем видео с ЮТУБ
-# следующие доработки, передавать видео куда-либо на сервер.!
-# в приложение на телевизор?
+# ПРОКСИ не нужен, только VPN на компе.
 
 
 import yt_dlp as youtube_dl
+import subprocess
+import time
 
-    # URL видео, которое вы хотите скачать
-url = 'https://www.youtube.com/watch?v=0tZ0SAcZtco'
+try:
+    subprocess.run(['ffmpeg', '-version'], check=True)
+    print("FFmpeg доступен и работает.")
+except subprocess.CalledProcessError:
+    print("FFmpeg не установлен или недоступен.")
 
-    # Параметры для youtube_dl
+
+# URL видео, которое вы хотите скачать
+url = 'https://www.youtube.com/watch?v=1rBjWMJIF20'
+
+# Параметры для youtube_dl
 ytdl_opts = {
-        'format': 'bestvideo[height<=720]+bestaudio/best[height<=720]',  # Формат видео (720p)
-        'outtmpl': '%(title)s.%(ext)s',  # Имя файла, в который будет сохранено видео
-        'noplaylist': True,  # Не загружать плейлисты, только одно видео
-        'socket_timeout': 30,
-    }
+    'format': 'bestvideo[height<=720]+bestaudio/best[height<=360]',  # Формат видео (720p)
+    # 'format': 'best',
+    'outtmpl': '%(title)s.%(ext)s',  # Имя файла, в который будет сохранено видео
+    'noplaylist': True,  # Не загружать плейлисты, только одно видео
+    'socket_timeout': 30,
+    'cookiesfrombrowser': ('chrome',),  # Использовать cookies из браузера Chrome ОЧЕНЬ ВАЖНО!!!!
+    # БРАУЗЕР ХРОМ Должен быть ЗАКРЫТ перед запуском скрипта
+    # 'cookiesfrombrowser': ('firefox',),  # Или использовать cookies из браузера Firefox
+    'http_headers': {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
+    },
+    # 'proxy': 'http://hWTxZZyr:cwywZYwS@176.113.42.26:63970',  # Укажите ваш прокси здесь
+    # Если требуется аутентификация, используйте 'proxy': 'http://username:password@proxyserver:port'
+    'postprocessors': [{  # Добавление постпроцессора для преобразования видео в mp4
+        'key': 'FFmpegVideoConvertor',
+        'preferedformat': 'mp4'  # Целевой формат
+    }],
+}
 
-    # Создаем объект youtube_dl
+ # Установка секундомера
+start_time = time.time()
+
+# Создаем объект youtube_dl
 with youtube_dl.YoutubeDL(ytdl_opts) as ydl:
-        ydl.download([url])
+    ydl.download([url])
+
+# Остановка секундомера и вывод времени выполнения
+end_time = time.time()
+execution_time = (end_time - start_time) / 60
+print(f"Время выполнения скрипта: {execution_time:.2f} минут")
